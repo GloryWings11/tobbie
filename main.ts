b33d7b357c81f912391fa9f3228a36ec5f28ea44
GloryWings11 <117503460+GloryWings11@users.noreply.github.com>
2022-11-05 17:05:39 +1100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+function Tobbie_Stop () {
+	
+}
+function Tobbie_Walk () {
+	
+}
+basic.forever(function () {
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        # . . . #
+        . # # # .
+        . . . . .
+        `)
+})
